1223d58bcd9a331bcd95406c757c0656a0fccaa4
SHJewell <scott.hjewell@gmail.com>
2023-01-16 21:58:21 -0800

    Added some .m3u to .m3u8 flexibility to reorient_playlist.py

--- reorient_playlist.py
+++ reorient_playlist.py
@@ -17,39 +17,53 @@ def repath_playlists(music_lib: str, read_path: str, export_path: str, ext: str)
     #dirs = os.listdir(o_path)
 
     for file in os.listdir(read_path):
+        ext_l = 4
+
         if 'Test' in file:
             continue
-        if '.m3u' in file:
-            print(file)
 
-            out_file_path = f'{export_path}/{file[:-4]}{ext}'
-            #print(out_file)
+        if '.m3u' == file[-4:]:
+            ext_l = 4
+        elif '.m3u8' == file[-5:]:
+            ext_l = 5
+        else:
+            continue
 
-            open_file = f'{playlist_path}{file}'
-            with io.open(open_file, errors='ignore') as f:
-                with open(out_file_path, "w") as g:
+        print(file)
 
-                    if ext == '.m3u8':
-                        g.write('#\n')
+        out_file_path = f'{export_path}/{file[:-ext_l]}{ext}'
 
-                    for line in f:
+        open_file = f'{playlist_path}{file}'
+        with io.open(open_file, errors='ignore') as f:
+            with open(out_file_path, "w") as g:
 
-                        line = line.replace('\n', '')
-                        line = line.replace('\r', '')
+                if ext == '.m3u8':
+                    g.write('#\n')
+
+                for line in f:
+
+                    if '/' not in line and '\\' not in line:
+                        continue
+
+                    line = line.replace('\n', '')
+                    line = line.replace('\r', '')
+                    if line.count('\\') > 2 or line.count('/') > 2:
                         album = f'{line[line.find("Albums") + len("Albums") + 1:]}\n'
+                    else:
+                        album = line + '\n'
 
-                        if '/' not in line:
-                            album = album.replace('\\', os.sep)
+                    if '/' not in line:
+                        album = album.replace('\\', os.sep)
 
-                        g.write(f'{music_lib}{album}')
+                    g.write(f'{music_lib}{album}')
 
-            f.close()
+        f.close()
 
 
 if __name__ == "__main__":
     music_lib = f'/media/disc1/Music/Albums/'
-    playlist_path = f'/media/disc1/Music/Playlists (copy)/'
-    export_path = f'/home/shjewell/Music/Playlists'
+    playlist_path = f'/home/shjewell/Music/Mobile Playlists/'
+    export_path = f'/home/shjewell/Music/New/'
     pl_ext = '.m3u'
 
     repath_playlists(music_lib, playlist_path, export_path, pl_ext)
